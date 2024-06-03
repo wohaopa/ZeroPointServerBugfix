d2a9d56357a9df6dc7665cf62ba92b774f1f73a4
初夏同学 <2411829240@qq.com>
2024-06-03 15:28:10 +0800

    init

---
+++ src/main/java/com/github/zeropoint/bugfix/ClientProxy.java
@@ -0,0 +1,8 @@
+package com.github.zeropoint.bugfix;
+
+public class ClientProxy extends CommonProxy {
+
+    // Override CommonProxy methods here, if you want a different behaviour on the client (e.g. registering renders).
+    // Don't forget to call the super methods as well.
+
+}
